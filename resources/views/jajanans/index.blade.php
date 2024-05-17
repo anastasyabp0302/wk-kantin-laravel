2b7b8b8910dee2536236fe9e7b45d2bc89705765
anastasyabp0302 <anastasyabp0302@gmail.com>
2024-05-17 15:19:10 +0700

    membuat tampilan menu

--- resources/views/jajanans/index.blade.php
+++ resources/views/jajanans/index.blade.php
@@ -3,27 +3,95 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kantin App</title>
+    <title>Wkantin</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <header>
-        <nav class="navbar">
-            <div class="navbar-logo">
-                <img src="images/logo-kantin.png" alt="Logo Kantin" width="50">
+    <div class="layout">
+        <div class="sidebar">
+        <img src="{{ asset('images/logo-kantin.png') }}" alt="Wkantin Wikrama School Logo">
+            <h2>jajanan</h2>
+        </div>
+        <div class="container">
+            <div class="search-bar">
+                <input type="text" placeholder="search....">
             </div>
-            <div class="navbar-menu">
-                <a href="#">Home</a>
-                <a href="#">Detail barang</a>
+            <div class="main-content">
+                <div class="card">
+                    <img src="{{ asset('images/sempol.jpg') }}" alt="Sempol">
+                    <p>Sempol</p>
+                    <p>Harga : Rp. 1500</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/cireng-isi.jpg') }}" alt="Cireng Isi">
+                    <p>Cireng Isi</p>
+                    <p>Harga : Rp. 2500</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/tahu-isi.jpg') }}" alt="Tahu Isi">
+                    <p>Tahu Isi</p>
+                    <p>Harga : Rp. 2000</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/pisang-aroma.jpg') }}" alt="Makanan 4">
+                    <p>Pisang Aroma</p>
+                    <p>Harga : Rp. 2000</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/onigiri.webp') }}" alt="Makanan 5">
+                    <p>Onigiri</p>
+                    <p>Harga : Rp. 3000</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/risoles.jpg') }}" alt="Makanan 5">
+                    <p>Risoles</p>
+                    <p>Harga : Rp. 300</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/srikaya-kue.jpg') }}" alt="Makanan 5">
+                    <p>Kue Srikaya</p>
+                    <p>Harga : Rp. 2000</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/Bakpao-pandan.webp') }}" alt="Makanan 5">
+                    <p>Bakpao Pandan</p>
+                    <p>Harga : Rp. 2500</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/brownies.jpg') }}" alt="Makanan 5">
+                    <p>Brownies</p>
+                    <p>Harga : Rp. 2500</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/dimsum.jpg') }}" alt="Makanan 5">
+                    <p>Dimsum</p>
+                    <p>Harga : Rp. 2500</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/bala-bala.jpg') }}" alt="Makanan 5">
+                    <p>Bala-Bala</p>
+                    <p>Harga : Rp. 1000</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/karoket-bihun.jpg') }}" alt="Makanan 5">
+                    <p>Karoket Bihun</p>
+                    <p>Harga : Rp. 2500</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/putu-ayu.jpg') }}" alt="Makanan 5">
+                    <p>Putu Ayu</p>
+                    <p>Harga : Rp. 1000</p>
+                </div>
+                <div class="card">
+                    <img src="{{ asset('images/pastel.jpg') }}" alt="Makanan 5">
+                    <p>Pastel</p>
+                    <p>Harga : Rp. 2500</p>
+                </div>
             </div>
-        </nav>
-    </header>
-    <main>
-    <title>Daftar Jajanan</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-
-<h1>Daftar Jajanan</h1>
-<a href="{{ route('jajanans.create') }}">Tambah Jajanan</a>
+        </div>
+    </div>
+</body>
+</html>
 
 @if ($message = Session::get('success'))
     <div>
@@ -31,20 +99,6 @@
     </div>
 @endif
 
-<table border="1">
-    <tr>
-        <th>Nama</th>
-        <th>Harga</th>
-        <th>Gambar</th>
-    </tr>
-    @foreach ($jajanans as $jajanan)
-    <tr>
-        <td>{{ $jajanan->nama }}</td>
-        <td>{{ $jajanan->harga }}</td>
-        <td><img src="{{ asset('images/' . $jajanan->gambar) }}" width="100"></td>
-    </tr>
-    @endforeach
-</table>
 </main>
 </body>
 </html>
